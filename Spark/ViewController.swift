783977b1393f6a3151f6511e974d038c721b0df3
Jason Yang <thejasonyang@gmail.com>
2017-02-25 18:29:15 -0500

    Initial Commit

---
+++ Spark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spark
+//
+//  Created by Jason Yang on 2/25/17.
+//  Copyright © 2017 Jason Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
